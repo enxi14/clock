39bd578be185e6e145cc58854db672cef577a68d
enxi14 <36089987+enxi14@users.noreply.github.com>
2023-09-27 17:14:10 -0400

    commit

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
-function updateClock(clockId, timeZone) {
-    const clock = document.getElementById(clockId);
-    const options = { timeZone, hour12: false, hour: '2-digit', minute: '2-digit', second: '2-digit' };
-    const currentTime = new Date().toLocaleTimeString('en-US', options);
-    clock.textContent = `${timeZone}: ${currentTime}`;
+function updateClock(timeZone, elementId) {
+    const now = new Date().toLocaleString("en-US", { timeZone });
+    document.getElementById(elementId).textContent = now;
 }
 
-function updateWorldClocks() {
-    // Update clocks for different time zones
-    updateClock('clock1', 'America/New_York');
-    updateClock('clock2', 'Europe/London');
-    // Add more clock updates as needed
+function updateWorldClock() {
+    updateClock("America/New_York", "new-york-time");
+    updateClock("Europe/London", "london-time");
+    updateClock("Asia/Tokyo", "tokyo-time");
 }
 
-// Update clocks every second
-setInterval(updateWorldClocks, 1000);
+// Update the clock every second
+setInterval(updateWorldClock, 1000);
 
 // Initial update
-updateWorldClocks();
+updateWorldClock();
